df2f10d2b1703a0ee1664db006af99b7c90c20bb
FabricioS <fab.silva.x2@gmail.com>
2020-08-28 14:59:10 -0300

    initial commit

--- novoa.sh
+++ novoa.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-read -p "Quantos arquivos quer criar ? " NOVO
-read -p "Qual o nome do arquivo ? " NOME
+read -p "Quantos quer criar ? " NOVO
+read -p "Qual o nome ? " NOME
 
-if [[ $NOVO = ?(+|-)+([0-9]) ]] #essa eu nao entendi só coloquei pra nao dar erro, pra checar se é inteiro
-then
-    for i in $(seq $NOVO); do
-    echo "#!/bin/bash" >"$NOME${i}.sh"
-    chmod +x "$NOME${i}.sh"
-    done
-else
-    echo "$NOVO não é um numero inteiro, so posso criar se você me disser um numero xD."
-fi
+read -p "Quer criar um diretorio ou um Arquivo [Aa > arquivo] [Dd > diretorio]? " ESCOLHA
+    case $ESCOLHA in
+        [Aa]* ) if [[ $NOVO = ?(+|-)+([0-9]) ]] #essa eu nao entendi só coloquei pra nao dar erro, pra checar se é inteiro
+                then
+                    for i in $(seq $NOVO); do
+                    echo "#!/bin/bash" >"$NOME${i}.sh"
+                    chmod +x "$NOME${i}.sh"
+                    done
+                else
+                    echo "$NOVO não é um numero inteiro, so posso criar se você me disser um numero xD."
+                fi
+                exit;;
+        [Dd]* ) if [[ $NOVO = ?(+|-)+([0-9]) ]] #essa eu nao entendi só coloquei pra nao dar erro, pra checar se é inteiro
+                then
+                    for i in $(seq $NOVO); do
+                    mkdir "$NOME${i}"
+                    done
+                else
+                    echo "$NOVO não é um numero inteiro, so posso criar se você me disser um numero xD."
+                fi
+                exit;; 
+        * ) echo "Por Favor responda Aa ou Dd"
+    esac
+
