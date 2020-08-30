1c1efbb94405230ad38818d728651be1e0b2e3bb
FabricioS <fab.silva.x2@gmail.com>
2020-08-29 21:34:23 -0300

    deletando arquivos desnescessarios

--- teste1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#verificando se é um numero inteiro
-# read -p "digite um numero" NOVO
-# if [ (($NOVO % 2 | bc... )) -eq0 ]
-# then 
-#     echo "inteiro"
-# else
-#     echo "digite um numero !"
-# fi
