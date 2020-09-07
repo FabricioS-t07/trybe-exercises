5f3fb644171f224fdb5d62c54eec2b59826a02c6
FabricioS <fab.silva.x2@gmail.com>
2020-09-07 14:15:10 -0300

    mude o texto da tag terminado

--- bloco_4/DIA_2/exercice_4.js
+++ bloco_4/DIA_2/exercice_4.js
@@ -1,8 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0 ;
+let soma = 0;
 
-for(let i = 0 ; i < numbers.length; i++){
-  soma = soma + numbers[i]
+for (let i = 0; i < numbers.length; i++) {
+	soma = soma + numbers[i];
 }
-let mediaArit = (soma / numbers.length) ;
-const maior20 = (mediaArit > 20) ? console.log("valor maior que 20") : console.log("valor menor ou igual a 20");
+let mediaArit = soma / numbers.length;
+const maior20 =
+	mediaArit > 20
+		? console.log('valor maior que 20')
+		: console.log('valor menor ou igual a 20');
--- bloco_5/dia_1/javascript_exercicio.js
+++ bloco_5/dia_1/javascript_exercicio.js
@@ -3,12 +3,31 @@
         - document.getElementById()
         - document.getElementsByClassName()
         - document.getElementsByTagName()
- Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
- Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
- Crie uma função que mude a cor do quadrado vermelho para branco.
- Crie uma função que corrija o texto da tag <h1>.
- Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
- Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+1 Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+2 Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+3 Crie uma função que mude a cor do quadrado vermelho para branco.
+4 Crie uma função que corrija o texto da tag <h1>.
+5 Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+6 Crie uma função que exiba o conteúdo de todas as tags <p> no console.
         */
+// exercício 1
+document.getElementsByTagName('p')[0].innerText =
+	'Ser um desenvolvedor capacitado e com experiência de 1 ano ';
+//exercício 2
+document.getElementsByClassName('center-content')[0].style.background =
+	'rgb( 76 , 164 , 109 )';
+//exercício 3
+document.getElementsByClassName('main-content')[0].style.background =
+	'rgb( 255 , 255 , 255 )';
+// backgroundBack.style.background = "white";
+//exercício 4
 
-document.getElementsByTagName("p")[0].innerText = "Ser um desenvolvedor capacitado e com experiência de 1 ano ";
+document.getElementsByTagName('h1')[0].innerText = 'Exercício 5.1 - Javascript';
+
+//exercicio 5
+
+for (i = 0; i < document.getElementsByTagName('p').length; i += 1) {
+	document.getElementsByTagName('p')[i].innerText = document
+		.getElementsByTagName('p')
+		[i].innerText.toUpperCase();
+}
